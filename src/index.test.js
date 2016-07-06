f9e58aa90a83576763055ca3eedfb565fe4d4232
vrbethi <ram.bethina@gmail.com>
2016-07-05 21:05:05 -0500

    feat(tests): add es6 tests & explore how modules work

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,22 @@
 import { assert } from 'chai';
 import starWars from './index';
+import { square, diag } from './feature_modules/lib';
 
 describe('all topics', function(){
   it('should list all', function(){
     assert.isDefined(starWars.all, 'returns an object');
+  });
+});
+
+describe('run square function', function(){
+  it('should return square of number', function(){
+    assert(square(2) === 4, 'Square of 2 should be 4');
+  });
+});
+
+describe('run diag function', function(){
+  it('should return diag of numbers', function(){
+    console.log('square root :'+diag(2, 2));
+    assert(diag(2, 2) === 2.8284271247461903, 'Square of 2 should be 4');
   });
 });
