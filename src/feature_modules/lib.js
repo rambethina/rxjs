5591851234be440848f22b317c128046ea6b2acd
vrbethi <ram.bethina@gmail.com>
2016-07-05 22:35:40 -0500

    feat(test lodash):

--- src/feature_modules/lib.js
+++ src/feature_modules/lib.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export const sqrt = Math.sqrt;
 
 export function square(x) {
@@ -8,3 +10,7 @@ export function diag(x, y) {
   return sqrt(square(x) + square(y));
 }
 //Named exports
+
+export function sizeOfArray(){
+  return _.size([1, 2, 3]);
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 import starWars from '../src/index';
-import { square, diag } from '../src/feature_modules/lib';
+import { square, diag, sizeOfArray } from '../src/feature_modules/lib';
 
 describe('all topics', function(){
   it('should list all', function(){
@@ -18,5 +18,12 @@ describe('run diag function', function(){
   it('should return diag of numbers', function(){
     console.log('square root :'+diag(2, 2));
     assert(diag(2, 2) === 2.8284271247461903, 'Square of 2 should be 4');
+  });
+});
+
+describe('test lodash', function(){
+  it('check size', function(){
+    console.log('Size of Array :'+sizeOfArray([2, 3, 4]));
+    assert(sizeOfArray([2, 3, 4]) === 3, 'Size of Array is 3');
   });
 });
